a7cb3625b169728ade2c3614f69ae0d2a98e66aa
yangc <yangc@mchz.com.cn>
2021-09-18 18:06:58 +0800

    add get-key

---
+++ db/oracle-getkey/src/app/chail/getkey/WriteMessegeToLog.java
@@ -0,0 +1,46 @@
+
+package app.chail.getkey;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class WriteMessegeToLog {
+	private static Calendar cale;
+	private static Date tasktime;
+	private static SimpleDateFormat df = new SimpleDateFormat(
+			"yyyy-MM-dd HH:mm:ss");;
+
+	public static void writeToLog(String messege,String fileName) {
+		String logFilePath = System.getProperty("user.dir") + File.separator
+				+ fileName;
+		File file = new File(logFilePath);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			FileWriter fw = new FileWriter(file, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			cale = Calendar.getInstance();
+			tasktime = cale.getTime();
+			System.out.println(messege);
+			bw.write(messege);
+			bw.newLine();
+
+			bw.flush();
+			bw.close();
+			fw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
